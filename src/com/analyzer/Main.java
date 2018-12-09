8e6cb84d1347fe0c8cb2e050cbd128b26fc723e4
Nicusor97 <nicolaepicatureanu@gmail.com>
2018-12-09 19:52:09 +0200

    added the java_cup code

--- src/com/analyzer/Main.java
+++ src/com/analyzer/Main.java
@@ -10,35 +10,37 @@ import java_cup.runtime.Symbol;
 public class Main {
     static public void main(String args[]) throws FileNotFoundException, UnsupportedEncodingException {
 
-        Lexer lex = null;
-//        PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
-        try {
-            lex = new Lexer(new FileReader("src/input.txt"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Symbol symbol = null;
-        do {
-            try {
-                symbol = lex.next_token();
-//                writer.println("We found the token : "+ lex.yytext() + ": with the ID: " + symbol);
-                System.out.println("We found : "+ lex.yytext() + " : with the ID: " + symbol);
-            } catch (Exception e) {
-                e.printStackTrace();
-//                writer.println(e);
-            }
-        } while (symbol.sym != 0);
+//        Lexer lex = null;
+////        PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+//        try {
+//            lex = new Lexer(new FileReader("src/input.txt"));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        Symbol symbol = null;
+//        do {
+//            try {
+//                symbol = lex.next_token();
+////                writer.println("We found the token : "+ lex.yytext() + ": with the ID: " + symbol);
+//                System.out.println("We found : "+ lex.yytext() + " : with the ID: " + symbol);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+////                writer.println(e);
+//            }
+//        } while (symbol.sym != 0);
 //        writer.close();
         
-//        try {
-//            parser p = new parser(new Lexer(new FileReader("src/input.txt")));
+        try {
+            parser p = new parser(new Lexer(new FileReader("src/input.txt")));
 //            Object result = p.parse().value;
-//       
-//          } catch (Exception e) {
-//        	  e.printStackTrace();
-//            } finally {
-//              /* do close out here */
-//              }
-//        } 
+            System.out.println("aaaaaa");
+            p.debug_parse();
+       
+          } catch (Exception e) {
+        	  e.printStackTrace();
+            } finally {
+              /* do close out here */
+              }
+        } 
     }
-}
+//}
